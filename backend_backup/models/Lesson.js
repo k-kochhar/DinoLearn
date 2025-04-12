a07d28be69c8b1e3397f28768150ea274cee4561
Joshua Attota <attotajva@gmail.com>
2025-04-12 18:17:09 -0400

    Backend Clearing

--- backend_backup/models/Lesson.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const lessonSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  overview: { type: String, required: true },
-  questions: [{ type: String, required: true }],
-});
-
-module.exports = mongoose.model('Lesson', lessonSchema);
--- backend_backup/models/Roadmap.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const roadmapSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  lessons: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Lesson' }]
-});
-
-module.exports = mongoose.model('Roadmap', roadmapSchema);
